4e3ae6923c788eae8da74140f00e189ca8d0a445
Gruzjan <gracjan122@protonmail.com>
2022-12-05 15:24:18 +0100

    z. 3, 4, 5

--- queries/Program.cs
+++ queries/Program.cs
@@ -86,25 +86,87 @@ namespace queries
             }
 
             Console.WriteLine("Migration ended");
-            zadanie1();
+            //zadanie1();
+            //zadanie2();
+            //zadanie3();
+            zadanie4();
+            //zadanie3();
         }
 
         static void zadanie1()
         {
             var db = ApplicationDbContext.Instance;
 
-            var klasy = db.Students
-                .GroupBy(k => k.Class)
+            var classes = db.Students
+                .GroupBy(s => s.Class)
                 .Select(s => new
                 {
-                    Dziewczyny = s.Count(c => c.FirstName.EndsWith("a")),
-                    Wszyscy = s.Count(),
-                    Klasa = s.Key
-                }).Where(x => x.Dziewczyny > x.Wszyscy / 2)
-                .Select(s => s.Klasa);
+                    girls = s.Count(s => s.FirstName.EndsWith("a")),
+                    everyone = s.Count(),
+                    Class = s.Key
+                }).Where(s => s.girls > s.everyone / 2)
+                .Select(s => s.Class);
 
-            Console.WriteLine($"Klasy: { String.Join(", ", klasy)}");
-
+            Console.WriteLine($"Classes: {String.Join(", ", classes)}");
         }
+
+        static void zadanie2()
+        {
+            var db = ApplicationDbContext.Instance;
+
+            var dates = db.Grades
+                .GroupBy(s => s.Date)
+                .Select(s => new
+                {
+                    date = s.Key,
+                    ones = s.Count(s => s.Value == 1)
+                }).Where(s => s.ones > 10)
+                .Select(s => s.date);
+
+            Console.WriteLine($"Dates: {String.Join(", ", dates)}");
+        }
+        static void zadanie3()
+        {
+            var db = ApplicationDbContext.Instance;
+
+            var averages = db.Grades
+                .Where(s => s.Subject.Name == "j.polski")
+                .Where(s => s.Student.Class.Contains("IV"))
+                .Select(s => s.Value)
+                .Average();
+            Console.WriteLine($"Avg: {Math.Round(averages, 2)}");
+        }
+        //something is wrong here man
+        static void zadanie4()
+        {
+            var db = ApplicationDbContext.Instance;
+
+            var fives = db.Grades
+                .GroupBy(x => x.SubjectId)
+                .Select(x => new
+                {
+                    count = x.Count(x => x.Value == 5),
+                    Subject = x.Key,
+                    Month = x.Select(x => x.Date.Month),
+                })
+                .GroupBy(x => x.Month);
+
+            Console.WriteLine(String.Join(", ", fives));
+        }
+        static void zadanie5()
+        {
+            var db = ApplicationDbContext.Instance;
+
+            var students = db.Grades.Where(x => x.Student.Class.Equals("II A"))
+                .Where(x => x.Subject.Name == "sieci komputerowe")
+                .Select(x => x.StudentId).ToList();
+            var studentsWithNoGrades = db.Students.Where(x => x.Class.Equals("II A"))
+                .Where(x => !students.Contains(x.StudentId))
+                .Select(x => $"{x.FirstName} {x.LastName}");
+
+            Console.WriteLine(String.Join(", ", studentsWithNoGrades));
+        }
+
+
     }
 }
